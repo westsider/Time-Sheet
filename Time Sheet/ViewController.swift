1c2dabc3a6f032cd8370ca5da74fbbb8b1d5dcf3
Warren Hansen <whansen1@mac.com>
2017-10-27 16:57:43 -0700

    initial commit

--- Time Sheet/ViewController.swift
+++ Time Sheet/ViewController.swift
@@ -10,16 +10,37 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var jobName: UIButton!
+    
+    @IBOutlet weak var totalForToday: UILabel!
+    
+    @IBOutlet weak var callTIme: UILabel!
+    
+    @IBOutlet weak var lunch: UILabel!
+    
+    @IBOutlet weak var wrapTime: UILabel!
+    
+    @IBOutlet weak var totalForJob: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func changeJobNameAction(_ sender: Any) {
     }
-
-
+    
+    @IBAction func changeCallAction(_ sender: Any) {
+    }
+    
+    @IBAction func changeLunchAction(_ sender: Any) {
+    }
+    
+    @IBAction func updateAction(_ sender: Any) {
+    }
+    
+    @IBAction func listJobsAction(_ sender: Any) {
+    }
 }
 
