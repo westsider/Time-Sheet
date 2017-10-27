4cc0827b147b5f75934e10f89010a24b8059c1f5
Warren Hansen <whansen1@mac.com>
2017-10-27 16:09:39 -0700

    Initial Commit

---
+++ Time Sheet/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Time Sheet
+//
+//  Created by Warren Hansen on 10/27/17.
+//  Copyright © 2017 Warren Hansen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
