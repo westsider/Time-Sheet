a0ff995b01339ea3f226b15e00c2bf7c121cc590
Warren Hansen <whansen1@mac.com>
2017-10-28 14:15:36 -0700

    task: added overtime charges

--- Time Sheet/ViewController.swift
+++ Time Sheet/ViewController.swift
@@ -19,8 +19,6 @@ class JobInfo {
 
 class ViewController: UIViewController, UITextFieldDelegate {
     
-    var jobInfo = JobInfo()
-    
     @IBOutlet weak var jobName: UITextField!
     
     @IBOutlet weak var totalForToday: UILabel!
@@ -33,12 +31,21 @@ class ViewController: UIViewController, UITextFieldDelegate {
     
     @IBOutlet weak var totalForJob: UILabel!
     
+    var jobInfo = JobInfo()
+    
+    var startTime:Date?
+    
+    var endTime:Date?
+    
+    //var overtimePay:Double?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         startTimeTextField.delegate = self
         lunchTextField.delegate = self
         wrapTextField.delegate = self
+        lunchTextField.text = "1"
     }
 
     @IBAction func dateFeildEditing(_ sender: UITextField) {
@@ -62,6 +69,8 @@ class ViewController: UIViewController, UITextFieldDelegate {
         
         startTimeTextField.text = dateFormatter.string(from: sender.date)
         
+        startTime = sender.date
+        
     }
     
     @IBAction func editLunch(_ sender: UITextField) {
@@ -78,6 +87,7 @@ class ViewController: UIViewController, UITextFieldDelegate {
         datePickerView.addTarget(self, action: #selector(ViewController.wrapPickerValueChanged), for: UIControlEvents.valueChanged)
     }
     
+////////////////////////////////////////
     @objc func wrapPickerValueChanged(sender:UIDatePicker) {
         
         let dateFormatter = DateFormatter()
@@ -88,6 +98,50 @@ class ViewController: UIViewController, UITextFieldDelegate {
         
         wrapTextField.text = dateFormatter.string(from: sender.date)
         
+        endTime = sender.date
+        
+        calcDailyEarnings()
+    }
+    
+    func calcDailyEarnings() {
+        var lunch = Int(lunchTextField.text!)
+        if lunch == 1 { lunch = 60 }
+        let seconds = endTime?.timeIntervalSince(startTime!)
+        var minuteTotal = Int(seconds! / 60) - lunch!
+        let hours = Int(minuteTotal / 60)
+        let minLeft = minuteTotal - ( hours * 60 )
+        if minuteTotal > 1500 { minuteTotal = 30 }
+        var display = ("\(hours) Hours, \(minLeft) min")
+        print(display)
+        if ( hours < 10)  {
+            display = "$1200"
+        } else {
+            
+            if ( hours <= 12 ) {
+                let timeAndHalf = Double(hours - 10)
+                //print("OT \(timeAndHalf)")
+                let timeAndHalfrate = 120 * 1.5
+                let overtimePay = timeAndHalf * timeAndHalfrate
+                //print("Overtime \(overtimePay)")
+                let totalPay = 1200 + overtimePay
+                print("OT \(timeAndHalf) Overtime \(String(describing: overtimePay)) total pay \(totalPay)")
+                display = ("\(hours):\(minLeft) $\(totalPay)")
+            }
+            
+            if ( hours > 12 ) {
+                //overtimePay = 360
+                let doubleTimeRate = Double( 120 * 2 )
+                let doubleTime = Double(hours - 12)
+                let overtimePay = 360 + ( doubleTime * doubleTimeRate )
+                let totalPay = 1200 + overtimePay
+                print("DT \(doubleTime) Overtime \(String(describing: overtimePay)) total pay \(totalPay)")
+                display = ("\(hours):\(minLeft) $\(totalPay)")
+            }
+            //MARK: - TODO calc minutes for ot
+            
+            
+        }
+        totalForToday.text = display
     }
     
     @IBAction func updateAction(_ sender: Any) {
